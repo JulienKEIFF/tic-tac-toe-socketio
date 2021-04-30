c585bec5142dfa3704f22f201ff4f21940669f42
bboime <benoit.boime@bluewire.fr>
2021-04-30 14:15:45 +0200

    change version of socketio

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+lib/index.js,a\8\a8a7a81ab4d6d49d6f2f41057a49003a5a2846c3
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const path = require('path');
 const app = express();
-const io = require('socket.io')(4001);
+const io = require("socket.io")(4001);
 const port = 4000;
 
 // Initialisation du jeu
@@ -13,17 +13,20 @@ const player = [
 ];
 const gameState = false;
 
-
-
-
-app.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'src', 'views/index.html'));
 });
 
-io.sockets.on("connection", socket => {
-    console.log('client connected');
-});
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'src', 'views/game.html'));
+})
 
+io.on("connection", socket => {
+    console.log('iic')
+    socket.on('game-join', client => {
+        console.log(client)
+    })
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
